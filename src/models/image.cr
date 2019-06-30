e219fa040bbd7723dcd8d4e7f490efdc2e628ebe
AndyRosenberg <andynaterosenberg@gmail.com>
2019-06-30 15:39:40 -0700

    Upload method

--- src/models/image.cr
+++ src/models/image.cr
@@ -11,4 +11,15 @@ class Image < Granite::Base
   def generate_key
     Random::Secure.hex(15)
   end
+
+  def upload(filepath : String)
+    client = Awscr::S3::Client.new(
+      ENV["AWS_REGION"],
+      ENV["S3_ACCESS_KEY_ID"],
+      ENV["S3_ACCESS_KEY_SECRET"]
+    )
+
+    body = File.read(File.expand_path(filepath))
+    client.put_object(ENV["AWS_BUCKET"], generate_key, body)
+  end
 end
