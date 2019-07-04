38744eb0b1f27d26dd2433012e604860644d822d
AndyRosenberg <andynaterosenberg@gmail.com>
2019-07-04 09:40:51 -0700

    class methods

--- src/models/image.cr
+++ src/models/image.cr
@@ -9,7 +9,7 @@ class Image < Granite::Base
   field object_url : String
   timestamps
 
-  def upload(filepath : String, body : String)
+  def self.upload(filepath : String, body : String)
     return false if body.blank?
 
     extension = (/\.([A-Za-z0-9]+)$/.match(filepath) || " ")[0].downcase
@@ -23,18 +23,18 @@ class Image < Granite::Base
                {"Content-Type" => "image/#{ext}"}
              )
 
-    update(object_key: key, object_url: get_url(key))
+    create(object_key: key, object_url: get_url(key))
   end
 
-  private def generate_key
+  private def self.generate_key
     UUID.random.to_s
   end
   
-  private def get_url(key : String)
+  private def self.get_url(key : String)
     "https://#{ENV["AWS_BUCKET"]}.s3-#{ENV["AWS_REGION"]}.amazonaws.com/#{key}"
   end
 
-  private def client
+  private def self.client
     Awscr::S3::Client.new(
       ENV["AWS_REGION"],
       ENV["S3_ACCESS_KEY_ID"],
@@ -42,7 +42,7 @@ class Image < Granite::Base
     )
   end
 
-  private def mimed(ext : String)
+  private def self.mimed(ext : String)
     return ext unless ext == "jpg"
     "jpeg"
   end
