fa8d95ca8f628e2426ad1abe971165e14909ab3a
AndyRosenberg <andynaterosenberg@gmail.com>
2019-07-08 22:39:47 -0700

    Took out bad mocks library

--- spec/models/user_spec.cr
+++ spec/models/user_spec.cr
@@ -1,33 +1,25 @@
 require "./spec_helper"
 require "../../src/models/user.cr"
 
-Mocks.create_mock User do
-  mock map_distance_from(other)
-end
-
 describe User do
   Spec.before_each do
     User.clear
   end
 
   describe "#distance_from" do
-    user_1 = User.create(city: "Chicago", state: "IL")
-    user_2 = User.create(city: "Phoenix", state: "AZ")
-
-    allow(user_1).to receive(maps_distance_from(user_2)).and_return(2821583)
+    user_1 = User.create(name: "1", email: "1", password: "", city: "Chicago", state: "IL")
+    user_2 = User.create(name: "2", email: "2", password: "", city: "Phoenix", state: "AZ")
 
     it "calculates distance" do
       user_1.current_distance_from?(user_2).should_not be_truthy
 
-      user_1.distance_from(user_2).should eq(2821583)
+      user_1.distance_from(user_2).should eq(2821145)
 
       user_1.current_distance_from?(user_2).should be_truthy
 
       user_2.update(city: "Flagstaff")
 
-      allow(user_1).to receive(maps_distance_from(user_2)).and_return(2820000)
-
-      user_1.distance_from(user_2).should_not eq(2821583)
+      user_1.distance_from(user_2).should_not eq(2821145)
     end
   end
 end
