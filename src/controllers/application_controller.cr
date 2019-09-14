f8f600a87181597e2395fcd891ae6a5bfcebb878
AndyRosenberg <andynaterosenberg@gmail.com>
2019-09-14 00:15:22 -0700

    Current user

--- src/controllers/application_controller.cr
+++ src/controllers/application_controller.cr
@@ -3,4 +3,11 @@ require "jasper_helpers"
 class ApplicationController < Amber::Controller::Base
   include JasperHelpers
   LAYOUT = "application.ecr"
+  @current_user : User | Nil
+
+  def current_user
+    return @current_user if @current_user
+    return false unless session[:user_id]
+    @current_user = User.find(session[:user_id])
+  end
 end
