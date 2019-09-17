11f0e879d8c0e675d8e9737939e1b4309f77d06f
AndyRosenberg <andynaterosenberg@gmail.com>
2019-09-16 22:30:21 -0700

    Add some bracing for message create

--- src/controllers/message_controller.cr
+++ src/controllers/message_controller.cr
@@ -1,5 +1,21 @@
 class MessageController < ApplicationController
   def create
+    if current_user
+      msg = Message.create(
+              conversation_id: params[:conversation_id],
+              user_id: current_user.try(&.id),
+              body: params[:body]
+            )
+      if msg.new_record?
+        flash[:danger] = "Failed to send message."
+      else
+        flash[:success] = "Message sent."
+      end
+    else
+      flash[:danger] = "Please log in to send message."
+    end
+
+    redirect_to "/conversations/#{params[:conversation_id]}"
   end
 
   def destroy
